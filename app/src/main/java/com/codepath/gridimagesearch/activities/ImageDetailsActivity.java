d10e7fd6671544bf2490e7a37753b4fe0a14a919
skipit <anonymous@anonymous.com>
2015-05-14 20:54:01 -0700

    Add Long Click Listener to share the Image

--- app/src/main/java/com/codepath/gridimagesearch/activities/ImageDetailsActivity.java
+++ app/src/main/java/com/codepath/gridimagesearch/activities/ImageDetailsActivity.java
@@ -1,14 +1,28 @@
 package com.codepath.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.codepath.gridimagesearch.R;
 import com.codepath.gridimagesearch.models.SearchResult;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ImageDetailsActivity extends ActionBarActivity {
 
@@ -20,9 +34,67 @@ public class ImageDetailsActivity extends ActionBarActivity {
         SearchResult result = (SearchResult) getIntent().getSerializableExtra("search_result");
 
         ImageView imageView = (ImageView) findViewById(R.id.ivImageResult);
+
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                // Get access to bitmap image from view
+                ImageView ivImage = (ImageView) findViewById(R.id.ivImageResult);
+                // Get access to the URI for the bitmap
+                Uri bmpUri = getLocalBitmapUri(ivImage);
+                if (bmpUri != null) {
+                    // Construct a ShareIntent with link to image
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                    shareIntent.setType("image/*");
+                    // Launch sharing dialog for image
+                    startActivity(Intent.createChooser(shareIntent, "Share Image"));
+                } else {
+                    // ...sharing failed, handle error
+                }
+
+                return true;
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("DEBUG", "Click");
+            }
+        });
+
         Picasso.with(this).load(result.url).into(imageView);
+
     }
 
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
